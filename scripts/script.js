ded68617794497e04c92fc87104a1e4c9c28313b
cdemar <demar.cory@gmail.com>
2021-05-22 11:09:27 -0700

    Commit 2.3

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,27 @@
 const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.navBar');
+const navBar = document.querySelector('.navBar');
 const menu = document.querySelector('.menu');
 const body = document.querySelector('body')
 const html = document.querySelector('html')
+const menuItems = document.querySelector('.menuItems')
+let menuOpen = false;
 
 
 hamburger.addEventListener('click', function(){
   this.classList.toggle('close');
-  nav.classList.toggle('revealNav');
+  navBar.classList.toggle('revealNav');
   menu.classList.toggle('revealItems');
   body.classList.toggle('stopMove')
   html.classList.toggle('stopMove')
 })
+
+/*
+hamburger.addEventListener('click', () => {
+  if (!menuOpen) {
+    hamburger.classList.add('open');
+    menuOpen = true;
+  } else {
+    hamburger.classList.remove('open');
+    menuOpen = false;
+  }
+}); */
