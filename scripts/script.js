e9937e38107fe7c0f1a9b1206d2854bf2f2610e2
cdemar <31713295+cdemar@users.noreply.github.com>
2021-05-24 13:03:50 -0700

    Commit 3.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,50 @@
 const hamburger = document.querySelector('.hamburger');
 const navBar = document.querySelector('.navBar');
 const menu = document.querySelector('.menu');
-const body = document.querySelector('body')
-const html = document.querySelector('html')
-const menuItems = document.querySelector('.menuItems')
-let menuOpen = false;
+const body = document.querySelector('body');
+const html = document.querySelector('html');
+const menuItemsAbout = document.querySelector('.menuItemsAbout');
+const menuItemsProjects = document.querySelector('.menuItemsProjects');
+const menuItemsContact = document.querySelector('.menuItemsContact');
+const menuItemsResume = document.querySelector('.menuItemsResume');
 
 
 hamburger.addEventListener('click', function(){
   this.classList.toggle('close');
   navBar.classList.toggle('revealNav');
   menu.classList.toggle('revealItems');
-  body.classList.toggle('stopMove')
-  html.classList.toggle('stopMove')
+  body.classList.toggle('stopMove');
+  html.classList.toggle('stopMove');
 })
 
-/*
-hamburger.addEventListener('click', () => {
-  if (!menuOpen) {
-    hamburger.classList.add('open');
-    menuOpen = true;
-  } else {
-    hamburger.classList.remove('open');
-    menuOpen = false;
-  }
-}); */
+menuItemsAbout.addEventListener('click', function(){
+  hamburger.classList.toggle('close');
+  navBar.classList.toggle('revealNav');
+  menu.classList.toggle('revealItems');
+  body.classList.toggle('stopMove');
+  html.classList.toggle('stopMove');
+})
+
+menuItemsProjects.addEventListener('click', function(){
+  hamburger.classList.toggle('close');
+  navBar.classList.toggle('revealNav');
+  menu.classList.toggle('revealItems');
+  body.classList.toggle('stopMove');
+  html.classList.toggle('stopMove');
+})
+
+menuItemsContact.addEventListener('click', function(){
+  hamburger.classList.toggle('close');
+  navBar.classList.toggle('revealNav');
+  menu.classList.toggle('revealItems');
+  body.classList.toggle('stopMove');
+  html.classList.toggle('stopMove');
+})
+
+menuItemsResume.addEventListener('click', function(){
+  hamburger.classList.toggle('close');
+  navBar.classList.toggle('revealNav');
+  menu.classList.toggle('revealItems');
+  body.classList.toggle('stopMove');
+  html.classList.toggle('stopMove');
+})
